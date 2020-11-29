22a65908a9b84bdb6ce4127efc4d2721235d3cc8
eitan baron <eitanbaron@outlook.com>
2020-11-29 08:25:36 +0200

    1

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import CoreuiVue from '@coreui/vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.performance = true
 Vue.use(CoreuiVue)
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 Vue.prototype.$log = console.log.bind(console)
 
 new Vue({
